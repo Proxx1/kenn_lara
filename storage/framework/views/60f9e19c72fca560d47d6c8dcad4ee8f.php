82c132de072171405cf458e8b3e07ad8602234b3
Proxx1 <166694113+Proxx1@users.noreply.github.com>
2025-03-31 23:02:07 +0800

    Add files via upload

---
+++ storage/framework/views/60f9e19c72fca560d47d6c8dcad4ee8f.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH C:\Users\kenji\Desktop\kenn_lara\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/f13e27aae1d595e258e590ff77f571ab.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH C:\Users\kenji\Desktop\kenn_lara\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
