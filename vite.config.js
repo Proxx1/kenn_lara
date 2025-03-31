6732645a21e110782c984a2fae45abab085a4e14
Proxx1 <166694113+Proxx1@users.noreply.github.com>
2025-03-31 23:05:21 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+    darkMode: 'class', // Enables dark mode support
+    plugins: [
+        laravel({
+            input: 'resources/js/app.jsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'resources/js'), // Ensures correct aliasing
+        },
+    },
+});
